951127cbfe7bb54dcb499ccec16eef71070c5b57
Mavis WONG <mavis.295@gmail.com>
2025-05-06 16:48:34 -0700

    Added docstring to the functions

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -3,6 +3,19 @@ import pandas as pd
 import numpy as np
 
 def load_data(file):
+    """
+    Loads an RDS file using pyreadr and returns the data as a pandas dataframe.
+    
+    Parameters
+    ----------
+    file : str
+        Path to the .rds file.
+
+    Returns
+    -------
+    pd.dataframe
+        Loaded dataset.
+    """
     result = pyreadr.read_r(file)
     
     return result[None]
--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,6 +2,24 @@ import pandas as pd
 import numpy as np
 
 def classify_species(x):
+    """
+    Classify site species type as 'Deciduous', 'Conifer', or 'Mixed' based on hardwood (HW) and softwood (SW) percentages.
+
+    A site is classified as:
+        - "Deciduous" if >= 80% of species are hardwood (HW),
+        - "Conifer" if >= 80% are softwood (SW),
+        - "Mixed" otherwise.
+
+    Parameters
+    ----------
+        x : str
+            A string containing HW:SW composition, formatted as 'HW <int>, SW <int>'.
+    
+    Returns
+    ----------
+    str
+        one of "Decidous", "Conifer" or "Mixed"
+    """
     hw = int(x.split(',')[0].strip('HW '))
     sw = int(x.split(',')[1].strip('SW '))
     sum = hw + sw
@@ -14,6 +32,24 @@ def classify_species(x):
 
 
 def data_cleaning(df):
+    """
+    This function performs the following data cleaning. 
+    1. Drop records of replanted sites
+    2. Drop rows with missing spectral indices.
+    3. Drop rows where the image year ('Year') precedes the planting year ('Season').
+    4. Drop rows with invalid survival rates (SrvvR_1, ..., SrvvR_7 not in [0, 100]).
+    5. Drop rows with invalid spectral index values (-1 to 1).
+
+    Parameters
+    ----------
+        df : pd.DataFrame
+            Original dataframe to be cleaned
+            
+    Returns
+    ----------
+        pd.DataFrame
+            Cleaned data.
+    """
     # Drop replanted sites
     df = df[(df['NmbrPlR'].isna()) | (df['NmbrPlR'] == 0)]
     # Drop rows with missing spectral indices
@@ -25,11 +61,11 @@ def data_cleaning(df):
 
     # drop out-of-range survival rates
     for i in ['SrvvR_1', 'SrvvR_2', 'SrvvR_3', 'SrvvR_4', 'SrvvR_5', 'SrvvR_6', 'SrvvR_7']:
-        df = df[(df[i] <= 100) | df[i].isna()]
+        df = df[(df[i].between(0, 100, inclusive='both')) | df[i].isna()]
 
     # drop out-of-range indices
     for i in ['NDVI', 'SAVI', 'MSAVI', 'EVI', 'EVI2', 'NDWI', 'NBR']:
-        df_train = df_train[df_train[i].between(-1, 1, inclusive='both')]
+        df = df[df[i].between(-1, 1, inclusive='both')]
     
     # Impute type column
     df.loc[df['NmbrPlR'].isna(
@@ -38,6 +74,22 @@ def data_cleaning(df):
     return df
 
 def data_split(df):
+    """
+    Splits the data into training and testing sets by site.
+    Each site is either in training set or in the test set. 
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe.
+
+    Returns
+    -------
+    pd.DataFrame
+        Training data containing records from 70% of sites.
+    pd.DataFrame
+        Testing data containing records from the remaining 30% sites.
+    """
     id_list = np.arange(df['ID'].min(), df['ID'].max()+1)
     # initialize split parameters
     SEED = 591
