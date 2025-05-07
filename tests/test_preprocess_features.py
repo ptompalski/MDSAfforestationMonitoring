80d1cc63c1beca50cb5cba4cf4998eebb75ea7d9
Benjamin Frizzell <benjamin.frizzell01@gmail.com>
2025-05-07 16:33:11 -0700

    add test coverage for classify_species()

--- tests/test_preprocess_features.py
+++ tests/test_preprocess_features.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
 import pandas as pd
-from ..src.data.preprocess_features import data_cleaning,classify_species
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.data.preprocess_features import classify_species,data_cleaning
 
 #columns for creating test data
 columns = [
@@ -16,7 +19,23 @@ def test_classify_species():
     '''
     Test for correctness of classify_species() function
     '''
-    pass
+    # testing typical use-cases
+    assert classify_species('HW 100, SW 0') == 'Decidous'
+    assert classify_species('HW 80, SW 20') == 'Decidous'
+    assert classify_species('HW 70, SW 30') == 'Mixed'
+    assert classify_species('HW 50, SW 50') == 'Mixed'
+    assert classify_species('HW 30, SW 70') == 'Mixed'
+    assert classify_species('HW 20, SW 80') == 'Conifer'
+    assert classify_species('HW 0, SW 100') == 'Conifer'
+    
+    # test erroneous use-cases
+    with pytest.raises(ValueError):
+        classify_species('an invalid string')
+        
+    with pytest.raises(ZeroDivisionError):
+        classify_species('HW 0, SW 0')
+        
+    
 
 def test_outlier_survival_rates():
     '''
