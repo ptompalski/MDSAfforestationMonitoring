993582ad98b22a9778b435360e4860c6976516d2
Benjamin Frizzell <benjamin.frizzell01@gmail.com>
2025-05-12 16:59:13 -0700

    implemented cross-validation wrapper

--- src/models/training_utils.py
+++ src/models/training_utils.py
@@ -1,13 +1,17 @@
 '''
-Various tools for training and cross-validating models.
+Tools for training and cross-validating models.
 '''
+import pandas as pd
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import RandomizedSearchCV,GridSearchCV, GroupKFold
 
 def cross_validation_wrapper(
     model_pipeline: Pipeline, 
+    df: pd.DataFrame,
     param_grid: dict,
     method: str = 'random',
-    n_iter: int = 10,
+    num_iter: int = 10,
+    num_folds: int = 5,
     random_state: int = 591
 ):
     '''
@@ -19,6 +23,10 @@ def cross_validation_wrapper(
     model_pipeline: sklearn.pipeline.Pipeline
         A model pipeline.
         
+    df: pd.DataFrame
+        The cleaned remote sensing training data.
+        Target survival rate should be converted to binary classes at this stage.xs
+    
     param_grid: dict
         Dictionary with parameters names (`str`) as keys and lists of
         parameter settings to try as values, or alternatively scipy.stats
@@ -37,7 +45,12 @@ def cross_validation_wrapper(
     random_state: int, default = 591
         Random seed for sampling parameter grid and cross-validation.  
     
-     Returns
+    num_folds: int, default = 5
+        Number of folds used in cross-validation.
+        Folds are split using `sklearn.model_selection.GroupKFold` to ensure sites
+        do not overlap between folds.
+    
+    Returns
     -------
     dict
         A dictionary containing the results of the hyperparameter tuning:
@@ -45,7 +58,43 @@ def cross_validation_wrapper(
         - 'best_score': the highest cross-validated score achieved
         - 'best_params': the parameter configuration corresponding to the best score
     '''
+    # check for correct method specification
+    if method not in ('random','grid'):
+        raise ValueError(
+            'method must be one of: {\'random\', \'grid\'}'
+            )
     
+    # get features and target
+    X = df.drop(columns='target'); y = df['target']
+    site_ids = df['ID']
     
+    if method == 'random':
+        cross_validator = RandomizedSearchCV(
+            estimator=model_pipeline,
+            param_distributions=param_grid,
+            n_iter=num_iter,
+            scoring = 'f1'
+            n_jobs=-1,
+            refit=True,
+            cv = GroupKFold(n_splits=num_folds)
+        )
+    else:
+        cross_validator = GridSearchCV(
+            estimator=model_pipeline,
+            param_grid=param_grid,
+            scoring = 'f1'
+            n_jobs=-1,
+            refit=True,
+            cv = GroupKFold(n_splits=num_folds)
+        )
     
+    cross_validator.fit(X,y,groups=site_ids)
+    
+    output_dict = {
+        'best_model': cross_validator.best_estimator_,
+        'best_score': cross_validator.best_score_,
+        'best_params': cross_validator.best_params_:
+    }
+    
+    return output_dict
     
