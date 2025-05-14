b55befc20d70d876a1d42e2faef28e172e4689b0
Mavis WONG <mavis.295@gmail.com>
2025-05-14 07:06:17 -0700

    Added unit tests for pivot_data functions

--- tests/test_pivot_data.py
+++ tests/test_pivot_data.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from ..src.data.pivot_data import pivot_df, mean_vi, match_vi, target_to_bin
 
-# Test data for pivot_df
-test_df = pd.DataFrame({
+# Test data for pivot_df()
+test_pivot_df = pd.DataFrame({
     'ID': [1, 2, 2],
     "PixelID": ['1_1', '2_2', '2_2'],
     "Season": [2001, 2002, 2002],
@@ -18,15 +18,14 @@ test_df = pd.DataFrame({
     'SrvvR_5': [None, 90, 90],
     'SrvvR_6': [90, None, None],
     'SrvvR_7': [101, None, None],
-    'AssD_1': [None, '2009-07-01', '2009-07-01'],
-    'AssD_2': ['2010-07-01']*3,
-    'AssD_3': [None, None, None],
-    'AssD_4': ['2012-07-01']*3,
-    'AssD_5': [None, '2013-07-01', '2013-07-01'],
-    'AssD_6': ['2014-07-01', None, None],
-    'AssD_7': ['2015-07-01', None, None],
-    'Year': [2001, 2002, 2003],
-    'DOY': [1]*3,
+    'AsssD_1': [None, '2009-07-01', '2009-07-01'],
+    'AsssD_2': ['2010-07-01']*3,
+    'AsssD_3': [None, None, None],
+    'AsssD_4': ['2012-07-01']*3,
+    'AsssD_5': [None, '2013-07-01', '2013-07-01'],
+    'AsssD_6': ['2014-07-01', None, None],
+    'AsssD_7': ['2015-07-01', None, None],
+    'ImgDate': ['2001-01-01', '2002-01-01', '2003-01-01'],
     'NDVI':  [0.3]*3,
     'SAVI':  [0.3]*3,
     'MSAVI': [0.3]*3,
@@ -39,35 +38,69 @@ test_df = pd.DataFrame({
     'TCW':   [0.3]*3
 })
 
+# Expected Columns after pivoting
 exp_pivot_cols = ['ID', 'PixelID', 'Density', 'Type',
            'Season', 'Age', 'target', 'SrvvR_Date']
 
-exp_pivot_df = pd.DataFrame(
+# Expected values in 'SrvvR_Date' after pivoting
+exp_pivot_date = ['2009-07-01', '2010-07-01', '2010-07-01',
+                  '2012-07-01', '2012-07-01', '2013-07-01', '2014-07-01']
+
+# Test data for match_vi()
+test_match_df = pd.DataFrame({
+    'ID': [1]*8 + [2]*8,
+    "PixelID": ['1_1']*8 + ['2_1']*4 + ['2_2']*4,
+    "Season": [2008]*16,
+    'Type': ['Conifer']*8 + ['Mixed']*8,
+    "Density": [1423.5]*8 + [2124.3]*8,
+    'SrvvR_1': [90]*16,
+    'SrvvR_2': [90]*16,
+    'SrvvR_3': [None]*16,
+    'SrvvR_4': [90]*16,
+    'SrvvR_5': [None]*16,
+    'SrvvR_6': [90]*16,
+    'SrvvR_7': [90]*16,
+    'AsssD_1': [None]*16,
+    'AsssD_2': ['2010-01-01']*16,
+    'AsssD_3': [None]*16,
+    'AsssD_4': ['2012-07-01']*16,
+    'AsssD_5': [None]*16,
+    'AsssD_6': ['2014-12-31']*16,
+    'AsssD_7': ['2015-12-31']*16,
+    'ImgDate': ['2009-12-15', '2009-12-16', '2010-01-17', '2010-01-18', '2014-11-28',
+                '2014-12-31', '2015-01-16', '2015-02-15']*2,
+    'NDVI':  [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'SAVI':  [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'MSAVI': [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'EVI':   [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'EVI2':  [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'NDWI':  [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'NBR':   [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'TCB':   [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'TCG':   [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2,
+    'TCW':   [0.1, 0.2, 0.3, 0.4, 0.85, 0.84, 0.74, 0.34]*2
+})
+
+# Pivoted dataframe
+pivoted_df = pd.DataFrame(
     {
-        'ID': [1, 1, 1, 2, 2, 2, 2],
-        'PixelID': ['1_1', '1_1', '1_1', '2_2', '2_2', '2_2', '2_2'],
-        'Density': [1434.54, 1434.54,  1434.54, 1235.0, 1235.0, 1235.0, 1235.0],
-        'Type':  ['Conifer', 'Conifer',  'Conifer', 'Mixed', 'Mixed', 'Mixed', 'Mixed'],
-        'Season': [2001, 2001, 2001, 2002, 2002, 2002, 2002],
-        'Age': [2, 4, 6, 1, 2, 4, 5],
-        'target': [90.0]*7,
-        'SrvvR_Date': ['2010-07-01', '2012-07-01', '2014-07-01',  '2009-07-01', '2010-07-01', '2012-07-01', '2013-07-01']
+        'ID': [1]*4 + [2]*8,
+        'PixelID': ['1_1']*4 + ['2_1']*4 + ['2_2']*4,
+        'Density': [1434.54]*4 + [2124.3]*8,
+        'Type':  ['Conifer']*4 + ['Mixed']*8,
+        'Season': [2008]*12,
+        'Age': [2, 4, 6, 7]*3,
+        'target': [90.0]*12,
+        'SrvvR_Date': ['2010-01-01', '2012-07-01', '2014-12-31', '2015-12-31']*3,
     }
 )
-exp_pivot_df['Age'] = exp_pivot_df['Age'].astype('int32')
-exp_pivot_df['target'] = exp_pivot_df['target'].astype('float32')
+# Spectral index columns
+cols = ['NDVI', 'SAVI', 'MSAVI', 'EVI',
+        'EVI2', 'NDWI', 'NBR', 'TCB', 'TCG', 'TCW']
 
-# Expected outlier to exclude
-df_outlier = pd.Series({
-    'ID': [1],
-    'PixelID': ['1_1'],
-    'Density': [1434.54],
-    'Type':  ['Conifer'],
-    'Season': [2001],
-    'Age': [7],
-    'target': [101],
-    'SrvvR_Date': ['2015-07-01']
-})
+# Expected average signals
+exp_avg = pd.concat([pd.Series([0.25, None, 0.79, None, 0.25, None, None, None, None, None, 0.79, None])]*10, axis=1)
+exp_avg.columns = cols
 
 # Test Data for target_to_bin()
 df_bin = pd.DataFrame(
@@ -78,122 +111,94 @@ df_bin = pd.DataFrame(
         'Type':  ['Conifer', 'Conifer', 'Mixed', 'Mixed', 'Decidous', 'Decidous'],
         'Season': ['2025', '2025', '2025', '2025', '2024', '2015'],
         'Age': ['1', '2', '3', '4', '5', '6'],
-        'target': ['100', '95.6', '80.0', '74.6', '40.3', '0.0'],
+        'target': [100, 95.6, 80.0, 74.6, 40.3, 0.0],
         'SrvvR_Date': ['2009-07-01', '2009-07-01', '2009-07-01', '2009-07-01', '2009-07-01', '2009-07-01']
     }
 )
+# Expected classified classes
+exp_bin = ['High', 'High', 'High', 'Low', 'Low', 'Low']
 
 
-def test_pivot_df():
+
+
+# 1. Tests for pivot_df()
+def test_pivot_df_size():
     '''
-    Test for if pivot_df() function correctly converts the target column into binary classes according to provided threshold.
+    Test if pivot_df() function returns the correct number of rows and columns.
     '''
-    pivoted_df = pivot_df(test_df)
-    
+    pivoted_df = pivot_df(test_pivot_df)
+
     assert pivoted_df.columns.to_list() == exp_pivot_cols
-    assert len(pivoted_df) == len(exp_pivot_df)
-    assert not pivoted_df.duplicated().all()
-    assert pivoted_df.sort_values(
-        by=['ID', 'Age']).reset_index(drop=True).equals(exp_pivot_df)
-    assert not (pivoted_df == df_outlier).all(axis=1).any()
-    assert not pivoted_df.isna().all()
-    assert pivoted_df['Age'].dtype == 'int64'
-    assert pivoted_df['target'].dtype == 'float32'
-    assert pivoted_df['SrvvR_Date'].dtype == 'datetime64[ns]'
+    assert len(pivoted_df) == 7
+
+def test_pivot_df_duplicates():
+    '''
+    Test if all duplicate rows are removed after pivoting.
+    '''
+    assert not pivot_df(test_pivot_df).duplicated().any()
+
+def test_pivot_df_na():
+    '''
+    Test if all rows with missing values are removed after pivoting.
+    '''
+    assert not pivot_df(test_pivot_df).isna().any(axis=None)
+
+def test_pivot_df_outlier():
+    '''
+    Test if outlier are removed after pivoting.
+    '''
+    assert (pivot_df(test_pivot_df)['Age'].isin([1, 2, 4, 5, 6])).all()
+
+def test_pivot_df_merge():
+    '''
+    Test if the assessment dates are matched correctly to the survival records.
+    '''
+    pivoted_df = pivot_df(test_pivot_df).sort_values('Age')
+    assert (pivoted_df['SrvvR_Date'] == exp_pivot_date).all()
 
 
-exp_match_df = pd.DataFrame(
-    {
-        'ID': [1]*4,
-        'PixelID': ['1_1']*4,
-        'Density': [1434.54]*4,
-        'Type':  ['Conifer']*4,
-        'Season': [2008]*4,
-        'Age': [2, 4, 6, 7],
-        'target': [90.0]*4,
-        'SrvvR_Date': ['2010-07-01', '2012-07-01', '2014-07-01', '2015-07-01'],
-        'NDVI':  [0.25, 0.75, 0.79, None],
-        'SAVI':  [0.25, 0.75, 0.79, None],
-        'MSAVI': [0.25, 0.75, 0.79, None],
-        'EVI':   [0.25, 0.75, 0.79, None],
-        'EVI2':  [0.25, 0.75, 0.79, None],
-        'NDWI':  [0.25, 0.75, 0.79, None],
-        'NBR':   [0.25, 0.75, 0.79, None],
-        'TCB':   [0.25, 0.75, 0.79, None],
-        'TCG':   [0.25, 0.75, 0.79, None],
-        'TCW':   [0.25, 0.75, 0.79, None]
-    }
-)
 
-pivoted_df = pd.DataFrame(
-    {
-        'ID': [1]*4,
-        'PixelID': ['1_1']*4,
-        'Density': [1434.54]*4,
-        'Type':  ['Conifer']*4,
-        'Season': [2008]*4,
-        'Age': [2, 4, 6, 7],
-        'target': [90.0]*4,
-        'SrvvR_Date': ['2010-07-01', '2012-07-01', '2014-07-01', '2015-07-01'],
-        'NDVI':  [0.25, 0.75, 0.79, None],
-        'SAVI':  [0.25, 0.75, 0.79, None],
-        'MSAVI': [0.25, 0.75, 0.79, None],
-        'EVI':   [0.25, 0.75, 0.79, None],
-        'EVI2':  [0.25, 0.75, 0.79, None],
-        'NDWI':  [0.25, 0.75, 0.79, None],
-        'NBR':   [0.25, 0.75, 0.79, None],
-        'TCB':   [0.25, 0.75, 0.79, None],
-        'TCG':   [0.25, 0.75, 0.79, None],
-        'TCW':   [0.25, 0.75, 0.79, None]
-    }
-)
-test_df = pd.DataFrame({
-    'ID': [1]*13,
-    "PixelID": ['1_1']*13,
-    "Season": [2008]*13,
-    'Type': ['Conifer']*13,
-    "Density": [1434.54]*13,
-    'SrvvR_1': [None]*13,
-    'SrvvR_2': [90]*13,
-    'SrvvR_3': [None]*13,
-    'SrvvR_4': [90]*13,
-    'SrvvR_5': [None]*13,
-    'SrvvR_6': [90]*13,
-    'SrvvR_7': [None]*13,
-    'AssD_1': [None]*13,
-    'AssD_2': ['2010-01-01']*13,
-    'AssD_3': [None]*13,
-    'AssD_4': ['2012-07-01']*13,
-    'AssD_5': [None]*13,
-    'AssD_6': ['2014-12-31']*13,
-    'AssD_7': [None]*13,
-    'Year': [2009, 2009, 2010, 2010, 2011, 2012, 2012, 2012, 2012, 2014, 2014, 2015, 2015],
-    'DOY': [50, 360, 3, 83, 183, 34, 170, 190, 318, 298, 361, 10, 19],
-    'NDVI':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'SAVI':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'MSAVI': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'EVI':   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'EVI2':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'NDWI':  [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'NBR':   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'TCB':   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'TCG':   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34],
-    'TCW':   [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.84, 0.74, 0.34]
-})
+# 2. Tests for match_vi()
+def test_match_vi_rows():
+    '''
+    Test if all rows in the pivot dataframe is returned after matching.
+    '''
+    pivoted_df['SrvvR_Date'] = pivoted_df['SrvvR_Date'].astype(
+        'datetime64[ns]').dt.date
+    assert match_vi(pivoted_df, test_match_df, 16)[
+        exp_pivot_cols].equals(pivoted_df)
 
-def test_mean_vi():
-    assert match_vi(pivoted_df, test_df, 16).equals(exp_match_df)
 
+def test_match_vi_cols():
+    '''
+    Test if the function returns the correct columns after matching.
+    '''
+    exp_match_cols = exp_pivot_cols + cols
+    assert match_vi(pivoted_df, test_match_df,
+                    16).columns.to_list() == exp_match_cols
+
+
+def test_match_vi_values():
+    '''
+    Test if the average values are calculated correctly.
+    '''
+    assert match_vi(pivoted_df, test_match_df, 16)[cols].equals(exp_avg)
+    
+    
+
+# 3. Tests for target_to_bin()
 def test_target_to_bin():
     '''
-    Test for if target_to_bin() function correctly converts the target column into binary classes according to provided threshold.
+    Test if the target column is correctly classified into binary classes.
     '''
-    df_80 = target_to_bin(df_bin, 80)
-    assert df_80['target'] == ['High', 'High', 'High', 'Low', 'Low', 'Low']
-    
-    # Test if Value Error is raised if threshold is not provided.
+    assert (target_to_bin(df_bin, 0.8)['target'] == exp_bin).all()
+
+
+def test_target_to_bin_exception():
+    '''
+    Test if ValueError is raised if threshold is not given
+    '''
     with pytest.raises(ValueError, match=r"Threshold for classifying survival rate required*"):
         target_to_bin(df_bin)
-
-
-
+    with pytest.raises(ValueError, match=r"The threshold must be between 0 and 1*"):
+        target_to_bin(df_bin, 80)
