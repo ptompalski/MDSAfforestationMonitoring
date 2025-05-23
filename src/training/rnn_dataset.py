e7144dc9f4dcfaf705dd522673c247e2470d0d75
Mavis WONG <mavis.295@gmail.com>
2025-05-23 07:51:55 -0700

    Add custom pytorch Dataset class

--- src/training/rnn_dataset.py
+++ src/training/rnn_dataset.py
@@ -4,3 +4,25 @@ from torch.nn.utils.rnn import pad_sequence
 import os
 import pandas as pd
 from typing import List, Tuple
+
+class AfforestationDataset(Dataset):
+    def __init__(self, lookup_dir : str, seq_dir : str):
+        self.lookup = pd.read_parquet(
+            lookup_dir).groupby(['age']).sample(frac=1)
+        self.seq_dir = seq_dir
+        self.site_cols = ['Density', 'Type_Conifer',
+                          'Type_Decidous', 'Type_Mixed', 'Age']
+        
+    def __len__(self):
+        return len(self.lookup)
+    
+    def __getitem__(self, idx : int):
+        row = self.lookup.iloc[idx]
+        seq_path = os.path.join(self.seq_dir, row['file_name'])
+        site_features = torch.tensor(row[self.site_cols].values, dtype=torch.float32)
+        target = torch.tensor(row['target'], dtype=torch.float32)
+        try:
+            sequence = torch.tensor(pd.read_parquet(seq_path), dtype=torch.float32)
+        except:
+            sequence = torch.zeros((1, 10), dtype=torch.float32)
+        return site_features, sequence, target
