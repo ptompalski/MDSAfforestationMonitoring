c387381dcd44c8ef4c713d53a444d772b9ffd10b
Benjamin Frizzell <benjamin.frizzell01@gmail.com>
2025-05-26 15:08:13 -0700

    add test coverage for process_single_site()

--- tests/test_get_time_series.py
+++ tests/test_get_time_series.py
@@ -52,6 +52,56 @@ def sample_interim_data():
     
     return interim_df
     
+@pytest.fixture
+def sample_lookup_data():
+    '''
+    Create sample lookup table for testing
+    '''
+    lookup_df = pd.DataFrame({
+        'ID': [1,2,3],
+        'PixelID': ['1_101','2_102','3_103'],
+        'SrvvR_Date': pd.to_datetime(['2020-06-01']*3),
+        'Age': [5,6,7],
+        'Density': [1200,1500,1800],
+        'Type': ['Mixed','Conifer','Decidous'],
+        'target': [70.0,80.0,90.0]
+    })
+    return lookup_df
+    
+@pytest.fixture
+def sample_remote_sensing_data():
+    '''
+    Create sample remote sensing table for testing.
+    '''
+    remote_df = pd.DataFrame({
+        'ID': [1,1,1,1,2,3],
+        'PixelID': ['1_102','1_101','1_101','1_101','2_102','3_103'],
+        'ImgDate': pd.to_datetime(['2019-01-01','2020-05-01','2020-06-01','2020-06-02','2019-01-01','2019-01-01']),
+        'NDVI':  [0.3,0.3,0.3,0.3,0.3,0.3],
+        'SAVI':  [0.3,0.3,0.3,0.3,0.3,0.3],
+        'MSAVI': [0.3,0.3,0.3,0.3,0.3,0.3],
+        'EVI':   [0.3,0.3,0.3,0.3,0.3,0.3],
+        'EVI2':  [0.3,0.3,0.3,0.3,0.3,0.3],
+        'NDWI':  [0.3,0.3,0.3,0.3,0.3,0.3],
+        'NBR':   [0.3,0.3,0.3,0.3,0.3,0.3],
+        'DOY': [1, 122, 153, 154,1,1],
+        'TCW': [14,16,18,20,22,24],  # (14,16,18,20,22,24] - 10) / 2 = [2.0,3.0,4.0,5.0,6.0,7.0]
+        'TCG': [16,18,20,22,24,26],  # ([[16,18,20,22,24,26] - 20) / 2 = [-2.0,-1.0,0,1.0,2.0,3.0]
+        'TCB': [33,39,45,51,57,63],   # ([33,39,45,51,57,63] - 30) / 3 = [1.0,3.0,5.0,7.0,9.0,11.0]
+        'NDVI': [0.3,0.3,0.3,0.3,0.3,0.3]
+    })
+    return remote_df
+    
+@pytest.fixture()
+def sample_norm_stats():
+    '''
+    Create sample normalization statistics dictionary for testing
+    '''
+    norm_stats = {
+        'mean': {'TCW': 10, 'TCG': 20, 'TCB': 30, 'Density': 1000},
+        'std': {'TCW': 2, 'TCG': 2, 'TCB': 3, 'Density': 100}
+    }
+    return norm_stats
 
 def test_get_summary_statistics():
     '''
@@ -151,122 +201,93 @@ def test_split_interim_datafram(sample_interim_data):
             .reset_index(drop=True)
         )
     )
+
+def test_process_single_site_with_records(sample_lookup_data,sample_remote_sensing_data,sample_norm_stats):
+    '''
+    Test to ensure records are filtered and normalized correctly if sequences exist for a site
+    '''
+    #Setup temporary output paths
+    tmp_output_dir = Path('tmp')
+    tmp_output_dir.mkdir(exist_ok=True)
     
-# def test_get_raw_sequence_filters_and_sorts():
-#     '''
-#     Test to ensure get_raw_sequence filters and sorts correctly
-#     '''
-#     df = pd.DataFrame({
-#         'ID': [1, 1, 1, 2],
-#         'PixelID': [101, 101, 101, 102],
-#         'ImgDate': pd.to_datetime(['2020-01-01', '2020-06-01', '2021-01-01', '2020-01-01']),
-#         'DOY': [1, 153, 1, 1],
-#         'NDVI': [0.5, 0.6, 0.7, 0.8]
-#     })
-
-#     site_key = {
-#         'ID': 1,
-#         'PixelID': 101,
-#         'SrvvR_Date': pd.Timestamp('2020-12-31')
-#     }
-
-#     result = _get_raw_sequence(site_key, df)
-
-#     expected = df.iloc[[0, 1]].sort_values(by="ImgDate").reset_index(drop=True)
-#     assert result.reset_index(drop=True).equals(expected)
-
-# def test_get_raw_sequence_returns_none_for_no_match():
-#     '''
-#     Test to ensure _get_raw_sequence can handle lookups with no matching sequences.
-#     '''
-#     df = pd.DataFrame({
-#         'ID': [2],
-#         'PixelID': [102],
-#         'ImgDate': pd.to_datetime(['2020-01-01']),
-#         'DOY': [1],
-#         'NDVI': [0.8]
-#     })
-
-#     site_key = {
-#         'ID': 1,
-#         'PixelID': 101,
-#         'SrvvR_Date': pd.Timestamp('2021-01-01')
-#     }
-
-#     assert _get_raw_sequence(site_key, df) is None
+    # prepare input for process_single_site
+    row_with_sequences = sample_lookup_data.iloc[0]
+    test_group_df = sample_remote_sensing_data.query("ID == 1 and PixelID == '1_101'")
+    
+    # get output
+    output_row_idx, output_filename = process_single_site(
+        row=(0,row_with_sequences),
+        group_df=test_group_df,
+        seq_out_dir=tmp_output_dir
+    )
+    
+    # check correctness of output
+    assert output_row_idx == 0
+    assert output_filename == '1_1_101_2020-06-01.parquet'
+    
+    # check that file was saved
+    assert os.path.exists(tmp_output_dir/'1_1_101_2020-06-01.parquet')
+    
+    # check filtering and time feature engineering
+    test_seq_data = pd.read_parquet(tmp_output_dir/'1_1_101_2020-06-01.parquet')
+    assert len(test_seq_data) == 2
+    assert np.allclose(
+        test_seq_data['neg_cos_DOY'].to_numpy(),
+        -np.cos(2 * np.pi * np.array([122, 153]) / 365)
+    )
+    assert np.allclose(
+        test_seq_data['log_dt'].to_numpy(),
+        np.log1p(np.array([31, 0]))
+    )
+    
+    # check columns are correctly dropped
+    assert set(test_seq_data.columns) == {
+        'NDVI', 'SAVI', 'MSAVI', 'EVI', 'EVI2', 'NDWI', 'NBR', 
+        'TCB', 'TCG', 'TCW','neg_cos_DOY','log_dt'
+    }
+    
+    # get rid of tmp directory once finished 
+    shutil.rmtree(tmp_output_dir)
+    
 
 def test_process_and_save_sequences():
     '''
     Test to ensure sequence files are saved properly and engineering is correct
     '''
+    pass
+
+    # # Setup temporary output paths
+    # tmp_output_dir = Path('tmp')
+    # tmp_output_dir.mkdir(exist_ok=True)
     
-    # create mock data
-    lookup_df = pd.DataFrame({
-        'ID': [1],
-        'PixelID': [101],
-        'SrvvR_Date': pd.to_datetime(['2020-06-01']),
-        'Age': [5],
-        'Density': [1200],
-        'Type': ['Mixed'],
-        'target': [1]
-    })
-
-    remote_df = pd.DataFrame({
-        'ID': [1],
-        'PixelID': [101],
-        'ImgDate': pd.to_datetime(['2020-01-01']),
-        'NDVI':  [0.3],
-        'SAVI':  [0.3],
-        'MSAVI': [0.3],
-        'EVI':   [0.3],
-        'EVI2':  [0.3],
-        'NDWI':  [0.3],
-        'NBR':   [0.3],
-        'DOY': [1],
-        'TCW': [14],  # (14 - 10) / 2 = 2.0
-        'TCG': [16],  # (16 - 20) / 2 = -2.0
-        'TCB': [33],   # (33 - 30) / 2 = 1.5
-        'NDVI': [0.6]
-    })
-
-    # mock norm stats
-    norm_stats = {
-        'mean': {'TCW': 10, 'TCG': 20, 'TCB': 30, 'Density': 1000},
-        'std': {'TCW': 2, 'TCG': 2, 'TCB': 2, 'Density': 100}
-    }
-
-    # Setup temporary output paths
-    tmp_output_dir = Path('tmp')
-    tmp_output_dir.mkdir(exist_ok=True)
+    # tmp_output_lookup_path = tmp_output_dir/ 'tmp_lookup.parquet'
     
-    tmp_output_lookup_path = tmp_output_dir/ 'tmp_lookup.parquet'
-    
-    # Call function
-    process_and_save_sequences(
-        lookup_df=lookup_df,
-        remote_sensing_df=remote_df,
-        seq_out_dir=tmp_output_dir,
-        lookup_out_path=tmp_output_lookup_path,
-        norm_stats=norm_stats
-    )
+    # # Call function
+    # process_and_save_sequences(
+    #     lookup_df=lookup_df,
+    #     remote_sensing_df=remote_df,
+    #     seq_out_dir=tmp_output_dir,
+    #     lookup_out_path=tmp_output_lookup_path,
+    #     norm_stats=norm_stats
+    # )
 
-    # Verify files exist, lookup and sequence.
-    saved_files = list(tmp_output_dir.glob("*.parquet"))
-    assert len(saved_files) == 2
+    # # Verify files exist, lookup and sequence.
+    # saved_files = list(tmp_output_dir.glob("*.parquet"))
+    # assert len(saved_files) == 2
 
-    # check columns and feature engineering
-    lookup_df_out = pd.read_parquet(tmp_output_lookup_path)
-    assert 'filename' in lookup_df_out.columns
-    assert np.isclose(lookup_df_out['Density'].iloc[0], 2.0)  # (1200 - 1000)/100
+    # # check columns and feature engineering
+    # lookup_df_out = pd.read_parquet(tmp_output_lookup_path)
+    # assert 'filename' in lookup_df_out.columns
+    # assert np.isclose(lookup_df_out['Density'].iloc[0], 2.0)  # (1200 - 1000)/100
 
-    # Read the sequence file and test engineering
-    sequence_df_out = pd.read_parquet(saved_files[0])
-    assert np.isclose(sequence_df_out['log_dt'].iloc[0], np.log1p(152))
-    assert np.isclose(sequence_df_out['neg_cos_DOY'].iloc[0], -np.cos(2 * np.pi * 1 / 365), atol=1e-6)
-    assert np.isclose(sequence_df_out['TCW'].iloc[0], 2.0)
-    assert np.isclose(sequence_df_out['TCG'].iloc[0], -2.0)
-    assert np.isclose(sequence_df_out['TCB'].iloc[0], 1.5)
+    # # Read the sequence file and test engineering
+    # sequence_df_out = pd.read_parquet(saved_files[0])
+    # assert np.isclose(sequence_df_out['log_dt'].iloc[0], np.log1p(152))
+    # assert np.isclose(sequence_df_out['neg_cos_DOY'].iloc[0], -np.cos(2 * np.pi * 1 / 365), atol=1e-6)
+    # assert np.isclose(sequence_df_out['TCW'].iloc[0], 2.0)
+    # assert np.isclose(sequence_df_out['TCG'].iloc[0], -2.0)
+    # assert np.isclose(sequence_df_out['TCB'].iloc[0], 1.5)
     
     
-    # get rid of tmp directory once finished 
-    shutil.rmtree(tmp_output_dir)
+    # # get rid of tmp directory once finished 
+    # shutil.rmtree(tmp_output_dir)
