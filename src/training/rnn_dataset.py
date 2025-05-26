07e6c8023b0bb583472cd1d2a6aa3244d03b9c19
Mavis WONG <mavis.295@gmail.com>
2025-05-26 05:37:57 -0700

    Fix conversion issues between pandas df and pytorch tensors

--- src/training/rnn_dataset.py
+++ src/training/rnn_dataset.py
@@ -85,17 +85,20 @@ class AfforestationDataset(Dataset):
         Load the satellite data and return a dictionary containing the site data, satellite data and target as Pytorch tensors.
         """
         row = self.lookup.iloc[idx]
-        seq_path = os.path.join(self.seq_dir, row['file_name'])
-        site_features = torch.tensor(row[self.site_cols].values, dtype=torch.float32)
+        seq_path = os.path.join(self.seq_dir, row['filename'])
+        site_features = torch.tensor(
+            row[self.site_cols].to_numpy('float32'))
         target = torch.tensor(row['target'], dtype=torch.float32)
         try:
-            sequence = torch.tensor(pd.read_parquet(seq_path, columns=self.seq_cols), dtype=torch.float32)
+            sequence = torch.tensor(pd.read_parquet(
+                seq_path, columns=self.seq_cols).to_numpy('float32'))
         except FileNotFoundError:
-            sequence = torch.zeros((1, len(self.seq_cols)), dtype=torch.float32)
+            sequence = torch.zeros(
+                (1, len(self.seq_cols)), dtype=torch.float32)
         return site_features, sequence, target
-         
 
-def collate_fn(batch : List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]):
+
+def collate_fn(batch: List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]):
     """
     Collate function for batching variable-length satellite sequences with their corresponding site features and targets.
 
@@ -134,10 +137,10 @@ def collate_fn(batch : List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]):
     sequence_length = [len(i) for i in sequence]
     sequence = pad_sequence(sequence, batch_first=True)
     return {
-        'site_features' : torch.stack(site_features),
-        'sequence' : sequence,
-        'target' : torch.stack(target),
-        'sequence_length' : torch.tensor(sequence_length, dtype=torch.int16)
+        'site_features': torch.stack(site_features),
+        'sequence': sequence,
+        'target': torch.stack(target),
+        'sequence_length': torch.tensor(sequence_length, dtype=torch.int16)
     }
 
 
