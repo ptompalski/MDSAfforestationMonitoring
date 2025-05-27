203cfcd4e1d5119e4d5be680f2bd4e6d57dc0495
Mavis Wong <77270525+MavisWong295@users.noreply.github.com>
2025-05-26 17:12:52 -0700

    Script and Tests for DataSet class, collation function and DataLoader (#65)
    * Create rnn_dataset.py
    
    * Add custom pytorch Dataset class
    
    * Add custom collate function for DataLoader
    
    * Add DataLoader wrapper to return DataSet and DataLoader
    
    * Add docstring for DataSet class
    
    * Add docstring for collation function
    
    * Add docstring for DataLoader wrapper
    
    * Rename dataloader wrapper function
    
    * Add reshuffle method to dataset class to enable randomised batches
    
    * Create tests for dataset class, dataloader and collation function
    
    * Add parameters to DataSet class to allow feature selection
    
    * Update docstrings
    
    * Exception Handling
    
    * Update default values for seq_cols
    
    * Update code to accept pathlike objects for lookup_dir and seq_dir
    
    * Add code to handle invalid batch_size, num_workers and pin_memory input in dataloader function
    
    * Fix conversion issues between pandas df and pytorch tensors
    
    * Update docstrings
    
    * Create test data for dataset class
    
    * Add fixtures to setup temporary test data directory and create mock dataset
    
    * Add tests for dataset class
    
    * Add tests and test data for collate_fn
    
    * Add tests for dataloader_wrapper
    
    * fix var name
    
    * Merge branch 'main' into rnn/dataset-dataloader
    
    * Update environment
    
    * Drop DOY, Type_Mixed columns

---
+++ src/training/rnn_dataset.py
@@ -0,0 +1,238 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+import os
+import pandas as pd
+from typing import List, Tuple
+import numpy as np
+
+
+class AfforestationDataset(Dataset):
+    """
+    A custom PyTorch Dataset for loading site records and their corresponding satellite time series records.
+
+    This dataset:
+    1. Loads a lookup table containing site records, target values, and the file names of associated satellite data stored as Parquet files.
+    2. Shuffles the lookup table within 'Age' groups to enable randomised batches while minimising sequence padding.
+    3. For each row in the lookup table, loads the corresponding variable-length satellite sequence.
+    4. Returns a tuple containing site features, satellite sequence, and target value as torch tensors.
+
+    Parameters
+    ----------
+    lookup_dir : str | os.PathLike
+        Path to the Parquet file containing the site features, target values, and file references.
+    seq_dir : str | os.PathLike
+        Directory containing the satellite data files. The 'filename' column in the lookup table specifies the file to load for each row.
+    site_cols : List of str
+        List of columns in the lookup table to be used as site features.
+    seq_cols : List of str
+        List of columns in the sequence to be used as satellite features. `len(seq_cols)` should match the `input_size` for the rNN model.
+
+    Attributes
+    ----------
+    original_lookup : pd.DataFrame
+        The full, unshuffled lookup DataFrame loading from the lookup parquet file.
+    lookup : pd.DataFrame
+        A shuffled copy of the lookup table, regenerated when `reshuffled()` is called.
+        Samples are randomly shuffled within 'Age' groups to optimise batch efficiency.
+
+    Methods
+    -------
+    reshuffle() : 
+        Regenerates a randomised lookup table by shuffling samples within 'Age' groups. Should be called at the start of each training epoch.
+
+    Returns
+    -------
+    Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+        - site_features : torch.Tensor of shape [num_site_features,]
+            Site records.
+        - sequence : torch.Tensor of shape [seq_len, input_size]
+            Variable-length satellite data.
+        - target : torch.Tensor of shape [1,]
+            Survival rate.
+
+    Notes
+    ------
+    A zero-filled tensor of shape (1, 10) is returned if the sequence file fails to load.
+    """
+
+    def __init__(
+        self,
+        lookup_dir: str | os.PathLike,
+        seq_dir: str | os.PathLike,
+        site_cols: List[str],
+        seq_cols: List[str]
+    ):
+
+        self.original_lookup = pd.read_parquet(lookup_dir)
+        self.seq_dir = seq_dir
+        self.site_cols = site_cols
+        self.seq_cols = seq_cols
+        self.reshuffle()
+
+    def __len__(self):
+        """
+        Return the number of samples in the dataset, that is the number of survival records in the lookup table.
+        """
+        return len(self.lookup)
+
+    def reshuffle(self):
+        """
+        Regenerates a randomised lookup table by shuffling samples within 'Age' groups.
+        """
+        self.lookup = self.original_lookup.groupby(
+            'Age').sample(frac=1, replace=False).reset_index(drop=True)
+
+    def __getitem__(self, idx: int):
+        """
+        Load the satellite data and return a tuple containing the site data, satellite data and target as Pytorch tensors.
+        """
+        row = self.lookup.iloc[idx]
+        seq_path = os.path.join(self.seq_dir, row['filename'])
+        site_features = torch.tensor(
+            row[self.site_cols].to_numpy('float32'))
+        target = torch.tensor(row['target'], dtype=torch.float32)
+        try:
+            sequence = torch.tensor(pd.read_parquet(
+                seq_path, columns=self.seq_cols).to_numpy('float32'))
+        except FileNotFoundError:
+            sequence = torch.zeros(
+                (1, len(self.seq_cols)), dtype=torch.float32)
+        return site_features, sequence, target
+
+
+def collate_fn(batch: List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]):
+    """
+    Collate function for batching variable-length satellite sequences with their corresponding site features and targets.
+
+    This function:
+    1. Sorts the batch in descending order by sequence length.
+    2. Calculate the length of each sequence.
+    3. Pads the sequences to the maximum seuquence length in the batch.
+    4. Stacks site features and target tensors.
+    5. Returns a dictionary with keys: `site_features`, `sequence`, `target` and `sequence_length`.
+
+    Parameters
+    ----------
+    batch : List of Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+        A list of tuples where each tuple corresponds to a sample in the batch, containing:
+            - site_features : torch.Tensor of shape [num_site_features,]
+                Site records.
+            - sequence : torch.Tensor of shape [seq_len, input_size]
+                Variable-length satellite data.
+            - target : torch.Tensor of shape [1,]
+                Survival rate.
+
+    Returns
+    --------
+    Dict[str, torch.Tensor]
+        - site_features : torch.Tensor of shape [batch_size, ]
+            Stacked site records.
+        - sequence : torch.Tensor of shape [batch_size, max_seq_len, input_size]
+            Padded satellite sequences.
+        - target : torch.Tensor of shape [batch_size,]
+            Stacked target values.
+        - sequence_length : torch.Tensor of shape [batch_size,]
+            Sequence lengths before padding.
+    """
+    batch = sorted(batch, key=lambda x: len(x[1]), reverse=True)
+    site_features, sequence, target = zip(*batch)
+    sequence_length = [len(i) for i in sequence]
+    sequence = pad_sequence(sequence, batch_first=True)
+    return {
+        'site_features': torch.stack(site_features),
+        'sequence': sequence,
+        'target': torch.stack(target),
+        'sequence_length': torch.tensor(sequence_length, dtype=torch.int16)
+    }
+
+
+def dataloader_wrapper(
+    lookup_dir: str | os.PathLike,
+    seq_dir: str | os.PathLike,
+    batch_size: int = 32,
+    num_workers: int = 0,
+    pin_memory: bool = True,
+    site_cols: List[str] = ['Density', 'Type_Conifer',
+                            'Type_Decidous', 'Age'],
+    seq_cols: List[str] = ['NDVI', 'SAVI',
+                           'MSAVI', 'EVI', 'EVI2', 'NDWI', 'NBR',
+                           'TCB', 'TCG', 'TCW', 'log_dt', 'neg_cos_DOY']
+):
+    """
+    Creates a PyTorch Dataset and DataLoader for batching satellite sequence data with site features.
+
+    This function:
+    1. Initializes a custom Dataset that loads site records and their corresponding satellite records.
+    2. Initiates a DataLoader using a custom collate function to handle variable-length sequences.
+
+    Parameters
+    ----------
+    lookup_dir : str | os.PathLike
+        Path to the Parquet file containing the lookup table.
+    seq_dir : str | os.PathLike
+        Directory containing the satellite data files referenced in the lookup table.
+    batch_size : int, default=32
+        Number of samples per batch. Expects a positive integer.
+    num_worker : int, default=0
+        Number of subprocesses to use for data loading. Expects a non-negative integer.
+    pin_memory : bool, default=True
+        If True, data is copied into device/CUDA pinned memory before returning. 
+    site_cols : List of str, default=['Density', 'Type_Conifer', 'Type_Decidous', 'Age']
+        List of columns in the lookup table to be used as site features.
+    seq_cols : List of str, default=['NDVI', 'SAVI', 'MSAVI', 'EVI', 'EVI2', 'NDWI', 'NBR', 'TCB', 'TCG', 'TCW', 'log_dt', 'neg_cos_DOY' ]
+        List of columns in the sequence to be used as satellite features. `len(seq_cols)` should match the `input_size` for the rNN model.
+
+    Returns
+    -------
+    Tuple[Dataset, DataLoader]
+        - dataset : Dataset
+            Instantiated custom Dataset for site and satellite data.
+        - loader : DataLoader
+            PyTorch DataLoader with batching and custom collation for variable-length sequences.
+
+    Raises
+    ------
+    ValueError
+        If any of the following conditions occur:
+        - `lookup_dir` or `seq_dir` is not a string or PathLike objects.
+        - `site_cols` or `seq_cols` is not a list of strings.
+
+    Notes
+    -----
+    - Shuffling is disabled to preserve sample order for efficient dynamic padding of variable-length sequences.
+    """
+    # Exception handling
+    for name, var, exp_type in [
+        ("lookup_dir", lookup_dir, (str, os.PathLike)),
+        ("seq_dir", seq_dir, (str, os.PathLike)),
+        ("site_cols", site_cols, list),
+        ("seq_cols", seq_cols, list),
+        ("batch_size", batch_size, int),
+        ("num_workers", num_workers, int),
+        ("pin_memory", pin_memory, bool)
+    ]:
+        if not isinstance(var, exp_type):
+            raise ValueError(
+                f'"{name}" expects {exp_type}, got {type(var).__name__}')
+        if exp_type == list and not all(isinstance(col, str) for col in var):
+            error_type = set(type(col).__name__ for col in var)
+            raise ValueError(
+                f'"{name}" expects a list of str, got {error_type}')
+    
+    # Fall back to default setting if invalid integer is supplied.
+    batch_size = max(32, batch_size)
+    num_workers = max(0, num_workers)
+    
+    dataset = AfforestationDataset(
+        lookup_dir=lookup_dir, seq_dir=seq_dir, site_cols=site_cols, seq_cols=seq_cols)
+    
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        collate_fn=collate_fn
+    )
+    return dataset, loader
---
+++ tests/test_rnn_dataset.py
@@ -0,0 +1,225 @@
+import shutil
+from pathlib import Path
+import pytest
+import numpy as np
+import pandas as pd
+import torch
+import os
+import sys
+from src.training.rnn_dataset import AfforestationDataset, collate_fn, dataloader_wrapper
+from typing import Tuple
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# Test data
+SITE_COLS = ['Density', 'Type_Conifer', 'Type_Decidous', 'Age']
+SEQ_COLS = ['NDVI', 'SAVI', 'MSAVI', 'EVI', 'EVI2', 'NDWI', 
+            'NBR', 'TCB', 'TCG', 'TCW', 'log_dt', 'neg_cos_DOY']
+EXP_SEQ_SHAPE = [(2, 12)]*3 + [(4, 12)]*2 + [(6, 12)] + [(1, 12)]
+
+mock_lookup_df = pd.DataFrame({
+    'ID': np.arange(1, 8),
+    'PixelID': np.arange(11, 18),
+    'SrvvR_Date': np.arange(2001, 2008),
+    'Age': [1, 1, 1, 2, 2, 3, 4],
+    'Density': [1431.4]*7,
+    'Type_Conifer': [0]*7,
+    'Type_Decidous': [0]*7,
+    'target': np.arange(71, 78),
+    'filename': ['mock_seq_1.parquet']*3 + ['mock_seq_2.parquet']*2 + ['mock_seq_3.parquet'] + ['na.parquet'],
+})
+
+mock_seq_data_1 = pd.DataFrame({
+    'ID': [1]*2,
+    "PixelID": [11]*2,
+    'ImgDate': ['2001-11-01']*2,
+    'NDVI':  [0.8, 0.6],
+    'SAVI':  [0.8, 0.6],
+    'MSAVI': [0.8, 0.6],
+    'EVI':   [0.8, 0.6],
+    'EVI2':  [0.8, 0.6],
+    'NDWI':  [0.8, 0.6],
+    'NBR':   [0.8, 0.6],
+    'TCB':   [0.8, 0.6],
+    'TCG':   [0.8, 0.6],
+    'TCW':   [0.8, 0.6],
+    'log_dt': np.arange(51, 53),
+    'neg_cos_DOY': np.arange(21, 23)
+})
+
+mock_seq_data_2 = pd.DataFrame({
+    'ID': [1]*4,
+    "PixelID": [11]*4,
+    'ImgDate': ['2001-11-01']*4,
+    'NDVI':  [0.8, 0.6]*2,
+    'SAVI':  [0.8, 0.6]*2,
+    'MSAVI': [0.8, 0.6]*2,
+    'EVI':   [0.8, 0.6]*2,
+    'EVI2':  [0.8, 0.6]*2,
+    'NDWI':  [0.8, 0.6]*2,
+    'NBR':   [0.8, 0.6]*2,
+    'TCB':   [0.8, 0.6]*2,
+    'TCG':   [0.8, 0.6]*2,
+    'TCW':   [0.8, 0.6]*2,
+    'log_dt': np.arange(53, 57),
+    'neg_cos_DOY': np.arange(23, 27)
+})
+
+mock_seq_data_3 = pd.DataFrame({
+    'ID': [1]*6,
+    "PixelID": [11]*6,
+    'ImgDate': ['2001-11-01']*6,
+    'NDVI':  [0.8, 0.6]*3,
+    'SAVI':  [0.8, 0.6]*3,
+    'MSAVI': [0.8, 0.6]*3,
+    'EVI':   [0.8, 0.6]*3,
+    'EVI2':  [0.8, 0.6]*3,
+    'NDWI':  [0.8, 0.6]*3,
+    'NBR':   [0.8, 0.6]*3,
+    'TCB':   [0.8, 0.6]*3,
+    'TCG':   [0.8, 0.6]*3,
+    'TCW':   [0.8, 0.6]*3,
+    'log_dt': np.arange(57, 63),
+    'neg_cos_DOY': np.arange(27, 33)
+})
+
+mock_batch = [
+    (torch.tensor([1, 2, 3, 4]),
+     torch.tensor([[1]*12, [2]*12]),
+     torch.tensor(71.0)),
+    (torch.tensor([5, 6, 7, 8]),
+     torch.tensor([[3]*12, [4]*12, [5]*12]),
+     torch.tensor(72.0))
+]
+
+exp_batch_out = {
+    'site_features': torch.tensor([[5, 6, 7, 8], [1, 2, 3, 4]]),
+    'sequence': torch.tensor([[[3]*12, [4]*12, [5]*12], [[1]*12, [2]*12, [0]*12]]),
+    'target': torch.tensor([72.0, 71.0]),
+    'sequence_length': torch.tensor([3, 2])
+}
+
+@pytest.fixture
+def setup_mock_data():
+    """
+    Create test parquet file
+    """
+    MOCK_SEQ_DIR = Path('tmp')
+    MOCK_SEQ_DIR.mkdir(exist_ok=True)
+    MOCK_LOOKUP_PATH = os.path.join(MOCK_SEQ_DIR, 'mock_lookup.parquet')
+    dfs = [(mock_lookup_df, 'mock_lookup.parquet'),
+           (mock_seq_data_1, 'mock_seq_1.parquet'), 
+           (mock_seq_data_2, 'mock_seq_2.parquet'), 
+           (mock_seq_data_3, 'mock_seq_3.parquet')]
+    for df, name in dfs:
+        df.to_parquet(os.path.join(MOCK_SEQ_DIR, name))
+    return MOCK_LOOKUP_PATH, MOCK_SEQ_DIR
+
+@pytest.fixture
+def mock_dataset(setup_mock_data):
+    """
+    Create mock dataset.
+    """
+    MOCK_LOOKUP_PATH, MOCK_SEQ_DIR = setup_mock_data
+    return AfforestationDataset(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, SITE_COLS, SEQ_COLS)
+
+
+
+# TESTS: Dataset Class
+def test_dataset_init(mock_dataset):
+    """
+    Test if dataset class initiates properly
+    """
+    dataset = mock_dataset
+    assert isinstance(dataset.original_lookup, pd.DataFrame)
+    assert isinstance(dataset.lookup, pd.DataFrame)
+    assert dataset.site_cols == SITE_COLS
+    assert dataset.seq_cols == SEQ_COLS
+
+def test_dataset_reshuffle(mock_dataset):
+    """
+    Test if the reshuffle function regenerates a randomised lookup table when called.
+    """
+    dataset = mock_dataset
+    lookup1 = dataset.lookup  # save original shuffled lookup table
+    dataset.reshuffle() # reshuffle lookup table
+    lookup2 = dataset.lookup
+    
+    # Allow 3 reshuffling attempts due to limited randomness in mock data.
+    fail_count = 0
+    while fail_count < 3:     
+        if lookup1.equals(lookup2):
+            dataset.reshuffle()
+            lookup2 = dataset.lookup
+            fail_count += 1
+        else:
+            break
+    assert not lookup1.equals(lookup2)
+    assert lookup1.shape == lookup2.shape
+    assert set(lookup1['ID']) == set(lookup2['ID'])
+    assert all(lookup2['Age'] == [1, 1, 1, 2, 2, 3, 4])
+
+def test_dataset_len(mock_dataset):
+    """
+    Test if function returns the length of lookup table when `len()` is used.
+    """
+    dataset = mock_dataset
+    assert len(dataset) == len(mock_lookup_df)
+
+def test_dataset_getitem(mock_dataset):
+    """
+    Test if dataset returns data and returns the data in the correct format.
+    """
+    data = mock_dataset
+    assert all(isinstance(idx, Tuple) for idx in data)
+    assert all(len(idx) == 3 for idx in data)
+    assert all(torch.is_tensor(i) for idx in data for i in idx)
+    assert all(len(idx[0]) == len(SITE_COLS) for idx in data)
+    assert [idx[1].shape for idx in data] == EXP_SEQ_SHAPE 
+    assert data[6][1].equal(torch.zeros((1, len(SEQ_COLS)), dtype=torch.float32)) # Fall back to zero if FileNotFound
+    assert all(idx[2].ndim == 0 for idx in data)  # Target tensor is a scalar
+    assert [idx[2].item() for idx in data] == data.lookup['target'].to_list()
+    
+
+
+# TESTS : Collation Function
+def test_collate_fn():
+    """
+    Test if collate function process the sequences correctly.
+    """
+    c_batch = collate_fn(mock_batch)
+    assert isinstance(c_batch, dict)
+    assert list(c_batch.keys()) == ['site_features', 'sequence', 'target', 'sequence_length']
+    assert all(isinstance(i, torch.Tensor) for i in list(c_batch.values()))
+    assert all(torch.equal(c_batch[i], exp_batch_out[i]) for i in c_batch)
+
+
+
+# TESTS: DataLoader
+def test_loader_exceptions(setup_mock_data):
+    """
+    Test if value error is raised if invalid dtypes are supplied.
+    """
+    MOCK_LOOKUP_PATH, MOCK_SEQ_DIR = setup_mock_data
+    invalid_lookup = ['invalid.parquet']
+    invalid_seq = ['tests']
+    with pytest.raises(ValueError):
+        dataloader_wrapper(invalid_lookup, MOCK_SEQ_DIR)  # string or pathlike
+        dataloader_wrapper(MOCK_LOOKUP_PATH, invalid_seq) # string or pathlike
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, site_cols='Age') # list
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, site_cols=['Age', 1])  # list of strings
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, seq_cols='NDVI')  # list
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, seq_cols=['NDVI', 1])  # list of strings
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, batch_size='1') # integer
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, num_workers='1') # integer
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, pin_memory='True') # boolean
+
+def test_loader_fallback(setup_mock_data):
+    """
+    Test if function overwrites invalid integer inputs for batch_size and num_workers.
+    """
+    MOCK_LOOKUP_PATH, MOCK_SEQ_DIR = setup_mock_data
+    dataset, loader = dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, batch_size=-1, num_workers=-1)
+    assert loader.batch_size == 32  # Default to 32 if batch_size is not a positive integer.
+    assert loader.num_workers == 0  # Default to 0 if num_workers is a negative integer.
+    
+    shutil.rmtree(MOCK_SEQ_DIR) # Remove temporary directory for test data
