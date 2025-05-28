d5a6e4789712183f72a04891bfeef2faff3bef0e
Mavis WONG <mavis.295@gmail.com>
2025-05-28 15:46:32 -0700

    Update tests to match changes to rnn_dataset

--- tests/test_rnn_dataset.py
+++ tests/test_rnn_dataset.py
@@ -203,15 +203,25 @@ def test_loader_exceptions(setup_mock_data):
     invalid_lookup = ['invalid.parquet']
     invalid_seq = ['tests']
     with pytest.raises(ValueError):
-        dataloader_wrapper(invalid_lookup, MOCK_SEQ_DIR)  # string or pathlike
-        dataloader_wrapper(MOCK_LOOKUP_PATH, invalid_seq) # string or pathlike
+        dataloader_wrapper(invalid_lookup, MOCK_SEQ_DIR, SITE_COLS, SEQ_COLS)  # string or pathlike
+        
+        dataloader_wrapper(MOCK_LOOKUP_PATH, invalid_seq,
+                           SITE_COLS, SEQ_COLS)  # string or pathlike
+        
         dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, site_cols='Age') # list
         dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, site_cols=['Age', 1])  # list of strings
+        
         dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, seq_cols='NDVI')  # list
         dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, seq_cols=['NDVI', 1])  # list of strings
-        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, batch_size='1') # integer
-        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, num_workers='1') # integer
-        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR, pin_memory='True') # boolean
+        
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR,
+                           SITE_COLS, SEQ_COLS, batch_size='1')  # integer
+        
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR,
+                           SITE_COLS, SEQ_COLS, num_workers='1')  # integer
+        
+        dataloader_wrapper(MOCK_LOOKUP_PATH, MOCK_SEQ_DIR,
+                           SITE_COLS, SEQ_COLS, pin_memory='True')  # boolean
 
 def test_loader_fallback(setup_mock_data):
     """
